360ca730417bec295ca4ebe68a2338a38c005a6c
Alimuhammad <noutkodera@gmail.com>
2025-10-01 18:23:53 +0300

    commit 2

--- src/edu_search.c
+++ src/edu_search.c
@@ -13,33 +13,22 @@
 #define UNUSED(x) (void)(x)
 
 size_t edu_linear_search(const void *ptr, size_t count, size_t size, const void *key, edu_cmp cmp) {
-    const char *base = (const char *)ptr;
+    const char *arr = (const char*)ptr;
     for (size_t i = 0; i < count; i++) {
-        const void *elem = base + i * size;
-        if (cmp(elem, key) == 0) {
-            return i; 
-        }
+        if (cmp(arr + i*size, key) == 0) return i;
     }
-    return (size_t)-1; 
+    return count; 
 }
 
 size_t edu_binary_search(const void *ptr, size_t count, size_t size, const void *key, edu_cmp cmp) {
-    size_t left = 0;
-    size_t right = count;
-    const char *base = (const char *)ptr;
-
+    size_t left = 0, right = count;
+    const char *arr = (const char*)ptr;
     while (left < right) {
-        size_t mid = left + (right - left) / 2;
-        const void *elem = base + mid * size;
-        int res = cmp(elem, key);
-        if (res == 0) {
-            return mid; 
-        } else if (res < 0) {
-            left = mid + 1;
-        } else {
-            right = mid;
-        }
+        size_t mid = left + (right - left)/2;
+        int c = cmp(arr + mid*size, key);
+        if (c == 0) return mid;
+        else if (c < 0) left = mid + 1;
+        else right = mid;
     }
-
-    return (size_t)-1; 
+    return count; 
 }
